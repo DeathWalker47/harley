6a7f706e4a94bebb5e49cdc41e73333002b38f25
DeathWalker47 <fizer_00@mail.ru>
2021-06-23 21:04:39 +0300

    added bikes section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
-$(function(){
+/* $(function(){
   
-});
+  
+
+}); */
+const swiper = new Swiper('.swiper-container', {
+    speed: 1000,
+    effect: 'fade',
+    fadeEffect: {
+    crossFade: true
+  },
+  zoom: {
+    maxRatio: 2,
+  },
+  /* autoplay: {
+    delay: 5000,
+  }, */
+    loop: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'true',
+      type: 'fraction',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+  });
+  
