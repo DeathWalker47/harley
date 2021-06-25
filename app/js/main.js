8d0f21b4a57ec7239d763227c01a098bd9865b88
DeathWalker47 <fizer_00@mail.ru>
2021-06-26 02:49:20 +0300

    fixed bikes section

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,7 @@ navHeight = nav.outerHeight();
 $(window).on('scroll', function () {
 const position = $(this).scrollTop();
 
+
 section.each(function () {
     const top = $(this).offset().top - navHeight - 5,
           bottom = top + $(this).outerHeight();
